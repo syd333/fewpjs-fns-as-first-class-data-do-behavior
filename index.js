276b4c2af3299b7a494b62ed4269cae33a2bd6fa
syd333 <sydmiranda@icloud.com>
2021-02-22 11:12:05 -0800

    commit tree 921de0f13da8692642f396c389e0b9f5f5e1b099

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString) {
+  timeString = timeString.split(":")
+ let int = parseInt(timeString)
+  if (int < 12) {
+    return "Good Morning"
+  } else if (int > 12 && int < 17) {
+    return "Good Afternoon"
+  } else if (int > 17) {
+    return "Good Evening"
+  }
+};
+
+function displayMessage(msg){
+  let h1 = document.getElementById("greeting")
+  h1.innerText = msg
+};
